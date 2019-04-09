cd96cfd025f4d1827343f628620a6d79656e6f1d
Wajdi Mousa <wajdimousasoft@gmail.com>
2019-04-09 14:07:16 +0300

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -17,6 +17,7 @@ pipeline {
     stages {
         stage("copy ufm") {
             steps {
+                checkout scm
                 sh """
                 cp /project/sw/ufm/install/UFM6.2.0/x86_64/rhel7/ufm-6.2.0-3.el7.x86_64.tgz /tmp
                 """
