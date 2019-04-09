e2967e553eb4e4c389ebd17e014b1859133163f2
Wajdi Mousa <wajdimousasoft@gmail.com>
2019-04-09 04:24:35 +0300

    Create test.groovy

---
+++ test.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/env groovy
+
+pipeline {
+    options { skipDefaultCheckout() }
+    parameters {
+        choice(
+                choices: "installed-" + env.BRANCH_NAME + "\nufm-svr43\nsmg-ib-svr033",
+                description: "Select setup:",
+name: 'setup')
+    }
+    agent {
+        node {
+            label params.setup
+            customWorkspace 'workspace/' + JOB_NAME
+        }
+}
+        stage("test") {
+            steps {
+                sh """
+                echo "wajdi Mousa"
+                """
+}
+
+}
