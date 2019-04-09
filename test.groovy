d28b3279f7ad1fade70e5f85397c999a61072b91
Wajdi Mousa <wajdimousasoft@gmail.com>
2019-04-09 05:21:25 +0300

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -4,7 +4,7 @@ pipeline {
     options { skipDefaultCheckout() }
     parameters {
         choice(
-                choices: "installed-" + env.BRANCH_NAME + "\nufm-svr43\nsmg-ib-svr033",
+                choices: "installed-" + env.BRANCH_NAME + "\nufm-svr43\nsmg-ib-svr033\nsmg-ib-svr042",
                 description: "Select setup:", 
                 name: 'setup')
     }
@@ -15,10 +15,19 @@ pipeline {
         }
     }
     stages {
-        stage("test") {
+        stage("copy ufm") {
             steps {
                 sh """
-                echo "wajdi Mousa"
+                cp /project/sw/ufm/install/UFM6.2.0/x86_64/rhel7/ufm-6.2.0-3.el7.x86_64.tgz /tmp
+                """
+            }
+        }
+        stage("extract ufm"){
+            steps{
+                sh """
+                cd /tmp
+                tar -xvzf ufm-6.2.0-3.el7.x86_64.tgz
+                cd  ufm-6.2.0-3.el7.x86_64
                 """
             }
         }
